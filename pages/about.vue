7ac8a9572e6427f8d77e7356ee28143f81ad6161
kulczy <sz.kulczy@gmail.com>
2019-04-03 22:46:13 +0200

    Initial

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    About
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    Contact
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
